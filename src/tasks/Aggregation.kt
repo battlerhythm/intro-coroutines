663c567fa57ce0fabc4f4fdfcda2fa12f3c59985
battlerhythm <16344965+battlerhythm@users.noreply.github.com>
2024-05-02 21:55:09 -0400

    task1 solution1

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -3,8 +3,6 @@ package tasks
 import contributors.User
 
 /*
-TODO: Write aggregation code.
-
  In the initial list each user is present several times, once for each
  repository he or she contributed to.
  Merge duplications: each user should be present only once in the resulting list
@@ -14,5 +12,16 @@ TODO: Write aggregation code.
  The corresponding test can be found in test/tasks/AggregationKtTest.kt.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
-fun List<User>.aggregate(): List<User> =
-    this
+fun List<User>.aggregate(): List<User> = solution1()
+
+private fun List<User>.solution1(): List<User> {
+    val map = mutableMapOf<String, Int>()
+    this.forEach {
+        map[it.login] = map.getOrDefault(it.login, 0) + it.contributions
+    }
+    val ans = mutableListOf<User>()
+    for ((login, contribution) in map) {
+        ans.add(User(login, contribution))
+    }
+    return ans.sortedByDescending { it.contributions }
+}
